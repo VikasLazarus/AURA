1701bdeecc05fed1ea978cf98899dec2c1d62167
VikasLazarus <148435329+VikasLazarus@users.noreply.github.com>
2025-09-03 23:58:30 -0700

    gp

--- data/glamorio-glamp.js
+++
@@ -1,8 +0,0 @@
-
-var myObject= {"title" : "Sucess For Dynamic Content", "discription" : "Doe", "keywords" : "Doe", "price" : "Doe",
-     "price-s" : "Doe", "rating" : "Doe"
-    , "reviews" : "Doe",  "location" : "Shimla",
-     "img1" : "placeholder.jpg" , "img2" : "placeholder.jpg" , "img3" : "placeholder.jpg" , "img4" : "placeholder.jpg" , "img5" : "placeholder.jpg"
-    ,"p1" : "Doe"
-};
-w3.displayObject("Glamorio-glamp", myObject);
